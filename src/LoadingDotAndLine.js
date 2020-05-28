b599ebd52e26160db89ba0433cdc5340d33bf5f0
Patrick San Juan <psj.webdev@gmail.com>
2020-05-27 20:16:21 -0700

    eslint prettier configured,  simple animation created

---
+++ src/LoadingDotAndLine.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./LoadingDotAndLine.css";
+
+const LoadingDotAndLine = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="svg8"
+      version="1.1"
+      viewBox="0 0 79.000778 22.116478"
+      height="22.116478mm"
+      width="79.000778mm"
+    >
+      <g transform="translate(-8.8859504,-7.8453839)">
+        <circle
+          id="loading-circle-1"
+          r="4.1622658"
+          cy="12.139941"
+          cx="13.180508"
+          fill="none"
+          stroke="#cccccc"
+          strokeWidth="0.264583"
+        />
+        <circle
+          id="loading-circle-2"
+          fill="none"
+          stroke="#cccccc"
+          strokeWidth="0.264583"
+          cx="26.01416"
+          cy="25.667305"
+          r="4.1622658"
+        />
+        <circle
+          id="loading-circle-3"
+          fill="none"
+          stroke="#cccccc"
+          strokeWidth="0.264583"
+          cx="38.154102"
+          cy="15.955353"
+          r="4.1622658"
+        />
+        <rect
+          id="loading-square-1"
+          y="12.139941"
+          x="49.253479"
+          height="7.9776764"
+          width="38.500961"
+          fill="none"
+          stroke="#cccccc"
+          strokeWidth="0.264583"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default LoadingDotAndLine;
